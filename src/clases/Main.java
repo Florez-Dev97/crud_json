a4ded78c3d296f488268f472b93223fd53e18280
Fernando Florez <florezf97@gmail.com>
2017-05-29 14:50:01 -0500

    Creación de método para buscar un objeto
    He creado un método para buscar un objeto dentro de un array que a su
    vez pertenece a un objeto. Su uso está ligado al método Select()
    
    Creacion de método para comprobar si una cadena es un JSONObject o un
    JSONArray.
    
    Creación de objeto de respuesta predefinida.

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,10 +23,26 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Manejador admin = new Manejador();
-        
+    
+     private int getTypeJSON(String js_obj) {
+        Object objeto;
         try {
+            objeto = new JSONObject(js_obj);
+            return 1;
+        
+        } catch(JSONException ex) {
+            try {
+                objeto = new JSONArray(js_obj);
+                return 2;
+                
+            } catch(JSONException ex2) {
+                return 0;
+            }
+        }  
+    }
+    public static void main(String[] args) {   
+        try {
+            Manejador admin = new Manejador();
             admin.setRuta("notebooks.json");
             admin.Conectar();
             admin.setNom_bd("notebooks");
@@ -35,13 +52,27 @@ public class Main {
             Date fecha = new Date();
             String date_now = format_date.format(fecha);
         
-            JSONObject cambio = new JSONObject("{nombre: \"Mi segunda libreta\", fecha_modificacion: \""+ date_now +"\"}");
             
+            JSONObject param_1 = new JSONObject("{nombre: Libreta}");
+            
+            JSONObject busqueda = admin.Select(param_1);
+            
+            if(busqueda.has("id") && busqueda.get("id").equals(0)) {
+                System.out.println(busqueda.toString(4));
+            
+            } else {
+                JSONObject into_array = new JSONObject("{ notes: [{id_note: 3}] }");
+                JSONObject nota = admin.SelectIntoArray(busqueda, into_array); 
+                
+                System.out.println(nota.toString(4));
+            }
+            
+            /*JSONObject cambio = new JSONObject("{nombre: Cronicas Rata, fecha_modificacion: \""+ date_now +"\"}");
             boolean exito = admin.update(cambio, new JSONObject("{id_notebook: 2}"));
             
             if(exito) {
                 System.out.println("Registro modificado de manera satisfactoria");
-            }
+            }*/
         } catch(FileNotFoundException fe) {
             fe.printStackTrace();
         
--- src/config/Manejador.java
+++ src/config/Manejador.java
@@ -28,10 +28,12 @@ public class Manejador {
     private Object[] campos;
     private File archivo;
     private String src_archivo;
+    private JSONObject dont_exists;
     
-    public Manejador() {
+    public Manejador() throws JSONException {
        this.ruta = ""; 
        this.src_archivo = "";
+       this.dont_exists = new JSONObject("{ id: 0, msg: Registro no encontrado }");
     }
 
     public String getRuta() {
@@ -74,7 +76,6 @@ public class Manejador {
         
         while( (ascii = is.read()) != -1 ) {
             this.src_archivo += (char) ascii;
-            this.src_archivo = this.src_archivo.trim();
         }
         is.close();
     }
@@ -98,25 +99,38 @@ public class Manejador {
     }
     
     /**
-     * @param parametros
-     * <p>Recibe los parametros de busqueda de la siguiente forma
-     * <ul>
-     *  <li>[</li>
-     *  <li>["campo", "valor"], ["campo", "valor"], ...</li>
-     *  <li>]</li>
-     * </ul>
-     * </p>
-     * @return 
-     * @throws org.json.JSONException
+     * @param str_obj 
+     * Recibe como parametro el objeto que se desea evaluar
+     * @return int<br />
+     * 0 si es un valor simple
+     * 1 si es un JSONObject
+     * 2 si es un JSONArray
     */
+    private int getTypeJSON(String str_obj) {
+        Object objeto;
+        try {
+            objeto = new JSONObject(str_obj);
+            return 1;
+        
+        } catch(JSONException ex) {
+            try {
+                objeto = new JSONArray(str_obj);
+                return 2;
+                
+            } catch(JSONException ex2) {
+                return 0;
+            }
+        }  
+    }
+    
     public JSONObject Select(JSONObject parametros) throws JSONException {
         // validar si el string es un object o un array
         JSONObject js_obj = new JSONObject( this.src_archivo );
         JSONArray js_array = (JSONArray) js_obj.get( this.nom_coleccion );
         
         JSONObject search;
-        JSONObject retorno = new JSONObject("{msg: \"Registro no encontrado\"}");
-
+        JSONObject retorno = this.dont_exists;
+        
         Iterator it_p = parametros.keys();
         String param = (String) it_p.next();
         
@@ -144,6 +158,74 @@ public class Manejador {
                 break;
             }
         }
+        
+        return retorno;
+    }
+    
+    public JSONObject SelectIntoArray(JSONObject objeto, JSONObject busqueda) throws JSONException {
+        JSONObject retorno = this.dont_exists;
+        
+        JSONArray js_array_obj;
+        JSONArray js_array_busqueda;
+        
+        Iterator it_json = objeto.keys();
+        Iterator it_busqueda = busqueda.keys();
+        
+        String key_busqueda = "", key_obj = "", key_arr_obj = "";
+        
+        int idx_busqueda = 0;
+        boolean fin = false;
+        
+        if(it_busqueda.hasNext()) {
+            key_busqueda = it_busqueda.next().toString();
+            js_array_busqueda = (JSONArray) busqueda.get(key_busqueda);
+            JSONObject obj_into_arr = js_array_busqueda.getJSONObject(0);
+            
+            Iterator it_key_to_search = obj_into_arr.keys();
+            if(it_key_to_search.hasNext()) {
+                String key_into_arr = it_key_to_search.next().toString();
+                String value_of_key = obj_into_arr.get(key_into_arr).toString();
+                
+                while( it_json.hasNext() ) {
+                    key_obj = it_json.next().toString();
+
+                    if(!key_obj.equals(key_busqueda)) {
+                        continue;
+                    }
+
+                    js_array_obj = (JSONArray) objeto.get(key_obj);
+
+                    for(int i = 0, l = js_array_obj.length(); i < l; i++) {
+                        JSONObject arr_obj = js_array_obj.getJSONObject(i);
+
+                        Iterator it_arr_obj = arr_obj.keys();
+
+                        while(it_arr_obj.hasNext()) {
+                            key_arr_obj = it_arr_obj.next().toString();
+                            String value_of_key_obj = arr_obj.get(key_arr_obj).toString();
+                            
+                            if( !key_arr_obj.equals(key_into_arr) || !value_of_key_obj.equals(value_of_key) ) {
+                                continue;
+                            }
+                            
+                            retorno = objeto;
+                            retorno.remove(key_obj);
+                            
+                            arr_obj.put("idx", idx_busqueda);
+                            retorno.put(key_obj, new JSONArray("["+ arr_obj +"]"));
+                            
+                            fin = true;
+                            break;
+                        }
+                        
+                        idx_busqueda++;
+                    }
+                    
+                    if(fin) break;
+                }
+            }
+        }
+        
         return retorno;
     }
     
@@ -155,7 +237,7 @@ public class Manejador {
         
         JSONObject busqueda = this.Select(criteria);
         
-        if( !busqueda.has("msg") ) {
+        if( !busqueda.equals(this.dont_exists) ) {
             Iterator it_changes = changes.keys();
             while( it_changes.hasNext() ) {
                 String key_c = it_changes.next().toString();
